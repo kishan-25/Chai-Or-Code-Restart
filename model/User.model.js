f52d05bbd4da8169a4a525854e4f327d0ffa6434
Balkishan <bkbajpay0609@gmail.com>
2025-12-09 12:25:20 +0530

    first schema bana liya

--- model/User.model.js
+++ model/User.model.js
@@ -1,6 +1,28 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema()
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String,
+    role: { // object kyo banaya taki or fields add kar sake
+        type: String,
+        enum: ["user", "admin"], // role me sirf ye do value aa sakti hai
+        default: "user" // default value user hai
+    },
+    isVerified: {
+        type: Boolean,
+        default: false // default value false hai taki pehle verify na ho
+    },
+    verificationToken: {
+        type: String
+    },
+    resetPasswordToken: {
+        type: String
+    },  
+    resetPasswordExpires: {
+        type: Date
+    }   
+}) // schema me aao and ek object banao and bhar do fields
 
 
 const User = mongoose.model("User", userSchema);
